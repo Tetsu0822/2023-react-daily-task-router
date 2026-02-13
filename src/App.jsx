3731dfd158233e62e957cbd60a975c891c463a00
Tetsu <ttgchang@gmail.com>
2026-02-13 10:26:20 +0800

    feat: 新增Post 頁面與路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
-import { HashRouter, NavLink, Routes, Route, useNavigate } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route, useNavigate, Outlet, useParams } from 'react-router-dom';
 
 const Home = () => {
   return <p>這是首頁</p>;
 }
 const Todo = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   return (<>
     <p>這是 Todo 頁面</p>
+    <button
+      type='button'
+      onClick={() => navigate('/post/post123')}
+    >
+      前往 Post 123
+    </button>
     <Logout />
   </>)
 };
@@ -26,7 +32,16 @@ const Logout = () => {
     >登出</button>
   </>)
 };
-
+const Post = () => {
+  return (<>
+    <p>這是 Post 頁面</p>
+    <Outlet />
+  </>)
+};
+const PostId = () => {
+  let param = useParams();
+  return <p>這是 Post 頁面裡的子路由，id 是 {param.id}</p>;
+};
 function App() {
   return (
     <div className="container">
@@ -44,6 +59,9 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
@@ -51,6 +69,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":id" element={<PostId />} />
+          </Route>
         {/* 練習區 */}
         </Routes>
       </HashRouter>
